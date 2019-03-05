903ffa7589344596787ec905c64a6d5f374ffe06
cd1992 <curtis.dirton@gmail.com>
2019-03-05 16:22:21 -0500

    Add files via upload

---
+++ I AM Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM Rich
+//
+//  Created by user150430 on 1/8/19.
+//  Copyright © 2019 Curtis Rashaun DIrton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
